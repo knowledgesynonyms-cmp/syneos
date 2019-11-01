4e45a297edf5b3ede284f81434868a742caff591
Surojit Basu <surojit119@gmail.com>
2019-11-01 11:52:22 +0530

    timbar template file pushed

---
+++ includes/views/timebar.tpl.php
@@ -0,0 +1,8 @@
+<div class="timebar">
+    <span class="fa fa-clock-o"></span>
+    <span class="ticker">00:00:00</span>
+    <span class="sep"></span>
+    <span class="playpause fa fa-pause-circle-o" title="Play/Pause"></span>
+    <span class="fa fa-question-circle-o"></span>
+    <button class="btn btn-danger btn-sm">COMPLETE</button>
+  </div>
